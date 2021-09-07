aaab8bc8778f9c3cce62edc27069278c276f8236
priti0802 <74915188+priti0802@users.noreply.github.com>
2021-09-07 11:37:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src ="https://cdn2.thecatapi.com/images/4li.gif";
+    div.appendChild(image);
+}
